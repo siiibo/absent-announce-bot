9a8eb3c543f24a043922ec8ea20a8f9845e8011f
Luliko_hub <snow-child@keio.jp>
2022-10-31 07:31:27 +0000

    feat: 課題2用ファイルの追加
    assigment.tsを追加

---
+++ src/assignment.ts
@@ -0,0 +1,53 @@
+
+   
+function isSameDate(date1: Date, date2: Date): boolean {
+    return date1.getMonth() === date2.getMonth() && date1.getDate() === date2.getDate();
+  }
+  
+  const date1: Date = new Date();
+  const date2: Date = new Date();
+  date2.setDate(date2.getDate() + 1);
+  const result: boolean = isSameDate(date1, date2);
+  console.log(result);
+  
+  import { addDays, setMilliseconds, setSeconds, setMinutes, setHours, nextMonday, startOfWeek, previousMonday, startOfMonth, addMonths} from 'date-fns'
+  
+  
+  function setMidnight(nowDate: Date){
+    const midnightDate = setMilliseconds(setSeconds(setMinutes(setHours(nowDate, 0), 0), 0), 0);
+    return midnightDate;
+  }
+  
+  
+  
+  function getStartEndDate(searchPeriod: string) {
+  
+    switch (searchPeriod) {
+      case "today": {
+        const startDate = setMidnight(new Date());
+        const endDate = addDays(startDate, 1);
+        return { start: startDate, end: endDate };
+      }
+      
+      case "week": {
+        const thisMonday = addDays(startOfWeek(new Date()), 1);
+        const startDate = setMidnight(thisMonday);
+        const endDate =  setMidnight(nextMonday(new Date()));
+        return { start: startDate, end: endDate };
+      }
+  
+      case "month": {
+        const startDate = setMidnight(startOfMonth(new Date()));
+        const endDate = addMonths(startDate, 1);
+        return { start: startDate, end: endDate };
+      }
+  
+    }
+  
+  }
+  const startEndDate= getStartEndDate("month");
+  
+  
+  console.log(startEndDate?.start); 
+  console.log(startEndDate?.end);
+  
